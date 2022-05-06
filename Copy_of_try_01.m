d77e8566acc48cc4dd24526acce381ebf8a9a140
kathy_kaffeine <eeprimakova@stud.etu.ru>
2022-05-06 13:55:43 +0300

    with tiles, wuthout color settings

--- Copy_of_try_01.m
+++ Copy_of_try_01.m
@@ -1,15 +1,28 @@
-V = matrix();
 x_3 = -15:0.1:14.9;
 y_3 = -14:0.1:13.9;
 z_3 = -2:0.1:38;
-
-x = 0; y = 0; z = 0;
+color_0 = [0.9290 0.6940 0.1250];
+color_1 = [0 0.4470 0.7410];
+colo2_2 = [1 0 0];
+color_3 = [0.4940 0.1840 0.5560];
+imagemap = [
+    color_0
+    color_1
+    color_2
+    color_3
+    ];
+x = 200; y = 0; z = 0;
+Vx = V(x,:,:);
+Vx = reshape(Vx, [280, 401]);
+Vx = Vx.*2;
 
 % отрисовка
-figure;
-s = slice(y_3, x_3, z_3, V, x, [], []); 
-set(s, 'EdgeColor','none');
-set(s, 'ColorMap', map);
+im = image(y_3, z_3, Vx);
+colormap(map);
+%colormap(map);
+%s = slice(y_3, x_3, z_3, V, x, [], []); 
+%ax1.YAxis.Visible = 'off'; % remove x-axis
+%s.FaceAlpha = 0.5;
 
 % скрипт для создания матрицы
 function [V] = matrix()
